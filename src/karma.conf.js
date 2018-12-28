7b2d09f317396ae49bc907aa6a5a23bdacd35d28
Bhargav <36185128+bhargavtejad@users.noreply.github.com>
2018-12-28 19:19:47 +0530

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require( 'karma-phantomjs-launcher' ),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-scss-preprocessor')
@@ -39,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
